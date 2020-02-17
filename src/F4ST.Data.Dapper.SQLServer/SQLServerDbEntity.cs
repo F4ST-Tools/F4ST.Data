17870625ece88abd6b6a94c6dcd52466ba00b242
Naser Rafinia <Naser.r@outlook.com>
2020-02-17 19:16:13 +0330

    Upgrade & Add SQL Server entity

---
+++ src/F4ST.Data.Dapper.SQLServer/SQLServerDbEntity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace F4ST.Data.Dapper.SQLServer
+{
+    public class SqlServerDbEntity : BaseEntity
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+    }
+}
